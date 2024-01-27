b31c813ff23cee41daba65328becaa01c3bc87c1
MrPowerpoint <tiziand4404@gmail.com>
2024-01-27 10:42:05 +0100

    Shader_for_Completed_room_and_Camera_Movement

--- Assets/01_scripts/SetShaderProp.cs
+++ Assets/01_scripts/SetShaderProp.cs
@@ -7,14 +7,23 @@ public class SetShaderProp : MonoBehaviour
     public Material mat;
     public string propertyName;
     public Transform player;
-    
+    public List<Material> materials;
 
     // Update is called once per frame
+   
+
     void Update()
     {
         if (player != null)
         {
-            mat.SetVector(propertyName, player.position);
+            foreach (Material mat in materials)
+            {
+                if (mat != null) { 
+                    mat.SetVector(propertyName, player.position);
+                }
+            }
+            
         }
     }
+    
 }
